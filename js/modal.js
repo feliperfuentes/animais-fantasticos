ee7cf394f961422ed93122e79e17aec4d1d602f8
Felipe Fuentes <feliperfuentes@gmail.com>
2025-07-20 18:36:25 -0300

    modal transformado em classe

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-export default function initModal() {
+/* export default function initModal() {
   const login = document.querySelector('[data-modal="abrir"]');
   const modalContainer = document.querySelector('[data-modal="container"]');
   const btnFechar = document.querySelector('[data-modal="fechar"]');
@@ -22,4 +22,40 @@ export default function initModal() {
       fecharModal();
     }
   });
+} */
+
+export default class Modal {
+  constructor(login, modalContainer, btnFechar) {
+    this.login = document.querySelector(login);
+    this.modalContainer = document.querySelector(modalContainer);
+    this.btnFechar = document.querySelector(btnFechar);
+    this.abrirModal = this.abrirModal.bind(this);
+    this.fecharModal = this.fecharModal.bind(this);
+    this.fecharModalFora = this.fecharModalFora.bind(this);
+  }
+
+  // ABRIR MODAL
+  abrirModal(event) {
+    event.preventDefault();
+    this.modalContainer.classList.add("modal-open");
+  }
+
+  //FECHAR MODAL PELO BOTÃO
+  fecharModal() {
+    this.modalContainer.classList.remove("modal-open");
+  }
+
+  //FECHAR MODAL CLICANDO FORA DELE
+  fecharModalFora(e) {
+    if (e.target === this.modalContainer) {
+      this.fecharModal();
+    }
+  }
+
+  init() {
+    this.login.addEventListener("click", this.abrirModal);
+    this.btnFechar.addEventListener("click", this.fecharModal);
+    this.modalContainer.addEventListener("click", this.fecharModalFora);
+    return this;
+  }
 }
